6586b881378f57b49bb240c328126047e4e7925a
Yuri Lemos Rodrigues <yuri.ylr@outlook.com>
2021-12-31 17:27:58 -0300

    day-02 part 2

--- day-02/src/lib.rs
+++ day-02/src/lib.rs
@@ -1,11 +1,9 @@
 #![allow(dead_code)]
 
-use std::vec::Splice;
-
 const INPUT_NAME: &'static str = "input.txt";
 
 
-fn calculate_course_plane(cmds: &Vec<String>) -> u32 {
+fn calculate_course_plane_part_1(cmds: &Vec<String>) -> u32 {
     let x = cmds.iter()
         .map(|s| match
             &*s.split(" ").collect::<Vec<&str>>() {
@@ -18,9 +16,31 @@ fn calculate_course_plane(cmds: &Vec<String>) -> u32 {
         )
         .fold((0, 0), |acc, n| match n {
             // (horizontal, death)
-            (1, unit) => (acc.0, acc.1 - unit),
-            (2, unit) => (acc.0, acc.1 + unit),
-            (3, unit) => (acc.0 + unit, acc.1),
+            (1, unit) => (acc.0, acc.1 - unit), // up 
+            (2, unit) => (acc.0, acc.1 + unit), // down
+            (3, unit) => (acc.0 + unit, acc.1), // forward
+            _ => acc
+        });
+
+    x.0 * x.1
+}
+
+fn calculate_course_plane_part_2(cmds: &Vec<String>) -> u32 {
+    let x = cmds.iter()
+        .map(|s| match
+            &*s.split(" ").collect::<Vec<&str>>() {
+                // (cmd, unit)
+                &["up", a] => (1, a.parse::<u32>().unwrap()),
+                &["down", b] => (2, b.parse::<u32>().unwrap()),
+                &["forward", c] => (3, c.parse::<u32>().unwrap()),
+                _ => (0, 0)
+            }        
+        )
+        .fold((0, 0, 0), |acc, n| match n {
+            // (horizontal, death, aim)
+            (1, unit) => (acc.0, acc.1, acc.2 - unit), // up 
+            (2, unit) => (acc.0, acc.1, acc.2 + unit), // down
+            (3, unit) => (acc.0 + unit, acc.1 + (acc.2 * unit), acc.2), // forward
             _ => acc
         });
 
@@ -40,7 +60,21 @@ mod tests {
             .list_lines();
         
             
-        let course_plane = calculate_course_plane(&lines);
+        let course_plane = calculate_course_plane_part_1(&lines);
+
+        assert_eq!(answer, course_plane);
+    }
+
+    #[test]
+    fn test_course_plane_part_2() {
+        let answer = 0;
+
+        let lines = utils::FileHolder::
+            build(INPUT_NAME).unwrap()
+            .list_lines();
+        
+            
+        let course_plane = calculate_course_plane_part_2(&lines);
 
         assert_eq!(answer, course_plane);
     }
